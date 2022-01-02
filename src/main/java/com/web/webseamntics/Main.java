6e4af1aaf792498b568265bd37ea21065617d48c
yahyamouman <yahyamouman@gmail.com>
2022-01-02 01:51:47 +0100

    Initial commit

---
+++ src/main/java/com/web/webseamntics/Main.java
@@ -0,0 +1,28 @@
+package com.web.webseamntics;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdfconnection.RDFConnection;
+import org.apache.jena.rdfconnection.RDFConnectionFactory;
+import org.apache.jena.vocabulary.VCARD;
+
+public class Main {
+    public static void main(String[] args){
+        Model model = ModelFactory.createDefaultModel();
+        // some definitions
+         String personURI    = "http://somewhere/JohnSmith";
+         String fullName     = "John Smith";
+        Resource johnSmith =
+                model.createResource(personURI)
+                        .addProperty(VCARD.FN, fullName);
+// ... build the model
+        String datasetURL = "http://localhost:3030/territoire";
+        String sparqlEndpoint = datasetURL + "/sparql";
+        String sparqlUpdate = datasetURL + "/update";
+        String graphStore = datasetURL + "/data";
+        RDFConnection conneg = RDFConnectionFactory.connect(sparqlEndpoint,sparqlUpdate,graphStore);
+        conneg.load(model); // add the content of model to the triplestore
+        conneg.update("INSERT DATA { <test> a <TestClass> }"); // add the triple to the triplestore
+    }
+}
---
+++ src/main/java/com/web/webseamntics/WebSeamnticsApplication.java
@@ -0,0 +1,13 @@
+package com.web.webseamntics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSeamnticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSeamnticsApplication.class, args);
+    }
+
+}
